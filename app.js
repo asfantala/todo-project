b6db583787d71dd0a97ef04e6816086c3ea59d0b
asfantala <22037049@student.ltuc.com>
2023-02-06 09:14:44 +0000

    todo-js

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict';
+// Ask the user to enter his/her name as input.
+var inputRes = prompt("Please enter your name: ");
+console.log(inputRes); 
+//Ask the user to enter his/her gender as input
+var inputGen = prompt("Enter your gender female/male");
+console.log(inputGen);
+//Ask the user to enter his/her age as input and alert the user if the age is less than or equal to zero
+var inputAge = prompt ("Enter your age");
+if (inputAge <= 0){
+    alert("The age less than or equal zero");
+}
+
+//Ask the user to confirm if he wants to skip the welcoming message.
+
+var confirMsgResult = confirm("press ok to see the welcoming message");
+console.log(confirMsgResult); 
+
+//Alert a welcoming message with the name of the user and the title Mr if the user is male and Ms if the user is female, and if the gender input is not correct alert the welcoming message without the title, according to the confirmation.
+
+/*if (confirMsgResult==true){
+if ( inputGen == "female"){
+alert ("Hello Ms" +" " +inputRes );
+}else if (inputGen == "male") {
+    alert ("Hello Mr" +" " + inputRes );  
+}  else {
+    alert ("Hello " +" " + inputRes );  
+
+} 
+}*/
+if (confirMsgResult==true){
+switch (inputGen){
+case "female" :
+alert ("Hello Ms" +" " +inputRes );
+break;
+case "male" :
+    alert ("Hello Mr" +" " +inputRes );
+    break;
+default :
+alert ("Hello " +" " + inputRes );
+
+
+}}
